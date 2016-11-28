4f92ff3326ea466762a1ee4857215a65be979306
ISHII 2bit <ishii@buffer-renaiss.com>
2016-11-28 20:23:59 +0900

    initial

---
+++ src/ofxAbletonLink.h
@@ -0,0 +1,84 @@
+//
+//  ofxOscPublisher.h
+//
+//  Created by ISHII 2bit on 2016/11/28.
+//
+//
+
+#pragma once
+
+#include <ableton/Link.hpp>
+#include "ofEvent.h"
+#include "ofEvents.h"
+#include "ofEventUtils.h"
+
+namespace ofx {
+    class AbletonLink {
+        ableton::Link link;
+        double beat{0.0};
+        double phase{0.0};
+        double bpm{120.0};
+        double quantum{4.0};
+    public:
+        ofEvent<double> bpmChanged;
+        ofEvent<std::size_t> numPeersChanged;
+        
+        AbletonLink()
+        : link(120.0f) {
+            link.enable(true);
+            link.setTempoCallback([&](double bpm) {
+                ofNotifyEvent(bpmChanged, bpm);
+            });
+            link.setNumPeersCallback([&](std::size_t peers) {
+                ofNotifyEvent(numPeersChanged, peers);
+            });
+            ofAddListener(ofEvents().update, this, &AbletonLink::update);
+        };
+        
+        double getBeat() const { return beat; }
+        void setBeat(double beat) {
+            const auto time = link.clock().micros();
+            auto timeline = link.captureAppTimeline();
+            timeline.requestBeatAtTime(beat, time, quantum);
+            link.commitAppTimeline(timeline);
+        }
+        void setBeatForce(double beat) {
+            const auto time = link.clock().micros();
+            auto timeline = link.captureAppTimeline();
+            timeline.forceBeatAtTime(beat, time, quantum);
+            link.commitAppTimeline(timeline);
+        }
+        
+        double getPhase() const { return phase; }
+        void setPhase(double phase) {
+            const auto time = link.clock().micros();
+            auto timeline = link.captureAppTimeline();
+        }
+        
+        double getBPM() const { return bpm; }
+        void setBPM(double bpm) {
+            this->bpm = bpm;
+            const auto time = link.clock().micros();
+            auto timeline = link.captureAppTimeline();
+            timeline.setTempo(bpm, time);
+            link.commitAppTimeline(timeline);
+        }
+        
+        std::size_t getNumPeers() const { return link.numPeers(); }
+        
+        void setQuantum(double quantum) { this->quantum = quantum; }
+        double getQuantum() const { return quantum; }
+        
+    private:
+        void update(ofEventArgs &) {
+            const auto time = link.clock().micros();
+            auto timeline = link.captureAppTimeline();
+            
+            beat = timeline.beatAtTime(time, quantum);
+            phase = timeline.phaseAtTime(time, quantum);
+            bpm = timeline.tempo();
+        };
+    };
+};
+
+using ofxAbletonLink = ofx::AbletonLink;
---
+++ src/ofxAbletonLinkThreaded.h
@@ -0,0 +1,49 @@
+//
+//  ofxAbletonLinkThreaded.h
+//
+//  Created by ISHII 2bit on 2016/11/28.
+//
+//
+
+#pragma once
+
+#include <ableton/Link.hpp>
+#include "ofEvent.h"
+#include "ofEvents.h"
+#include "ofEventUtils.h"
+
+namespace ofx {
+    class AbletonLinkThreaded {
+        std::atomic<bool> finish{false};
+        ableton::Link link;
+        std::thread thread;
+        
+    public:
+        AbletonLinkThreaded()
+        : link(120.0f)
+        , thread([&]() {
+            while(!finish) {
+                const auto time = link.clock().micros();
+                auto timeline = link.captureAppTimeline();
+                const auto beats = timeline.beatAtTime(time, 4.0);
+                const auto phase = timeline.phaseAtTime(time, 4.0);
+                std::cout << "info\n"
+                << "  beats: " << beats << std::endl
+                << "  phase: " << phase << std::endl
+                << "  tempo: " << timeline.tempo() << std::endl
+                << "  peers: " << link.numPeers() << std::endl;
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            }
+        }) {
+            link.enable(true);
+            ofAddListener(ofEvents().exit, this, &AbletonLinkThreaded::exit, OF_EVENT_ORDER_BEFORE_APP);
+        };
+
+        void exit(ofEventArgs &) {
+            finish = true;
+            thread.join();
+        }
+    };
+};
+
+using ofxAbletonLinkThreaded = ofx::AbletonLinkThreaded;
